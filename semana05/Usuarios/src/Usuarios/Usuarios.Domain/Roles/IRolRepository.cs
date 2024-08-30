7898b8893f359801fb7d07b2d98cab3e46860f53
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-08-29 22:18:26 -0500

    semana 05

---
+++ semana05/Usuarios/src/Usuarios/Usuarios.Domain/Roles/IRolRepository.cs
@@ -0,0 +1,8 @@
+namespace Usuarios.Domain.Roles;
+
+public interface IRolRepository
+{
+    Task<Rol?> GetByNameAsync(string rolName, CancellationToken cancellationToken = default);
+
+    Task<List<Rol>> ListAsync(CancellationToken cancellationToken = default);
+}
