836efb279324c27011ae8adb9dfc9ce657b79e75
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-09-03 21:42:37 -0500

    semana 7

---
+++ semana07/Usuarios/src/Usuarios/Usuarios.Domain/Usuarios/IUsuarioRepository.cs
@@ -0,0 +1,9 @@
+namespace Usuarios.Domain.Usuarios;
+
+public interface IUsuarioRepository
+{
+    Task<Usuario?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(Usuario usuario);
+
+}
